027ac1d6dde2439a763e044f7f7e69fb2f328eec
Ian Avery <ianedavery@gmail.com>
2018-05-19 13:10:15 -0700

    added logout button to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import RequiresLogin from './RequiresLogin';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 
 class Dashboard extends Component {
+
+	logOut() {
+		this.props.dispatch(clearAuth());
+		clearAuthToken();
+	}
+
 	render() {
 		return (
-			<div>Hello</div>
+			<div>
+				Hello
+				<button type='button' onClick={() => this.logOut()}>Logout</button>
+			</div>
 		)
 	}
 }
