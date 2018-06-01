7386bef41363d32ed3d4763e9de84870b1a35934
Ian Avery <ianedavery@gmail.com>
2018-06-01 11:10:16 -0700

    added styling to landing page

---
+++ src/components/Box.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {TweenMax} from "gsap/TweenMax";
+
+class Box extends Component {
+
+	  componentWillEnter (callback) {
+	    const el = this.container;
+	    const el2 = this.container2
+	    TweenMax.staggerFromTo(el, 1, {x: 1000, y: 0}, {x: 0, y: 0, onComplete: callback});
+	  }
+
+	  componentWillLeave (callback) {
+	    const el1 = this.container1;
+	    const el2 = this.container2;
+	    const el3 = this.container3;
+	    const el4 = this.container4;
+	    const el5 = this.container5;
+	    TweenMax.staggerFromTo([el1, el2, el3, el4, el5], 2, {x: 0, y: 0}, {x: 1000, y: 0, onComplete: callback}, 0.1);
+	  }
+
+	render() {
+
+		return (
+
+			<div>
+				<div className='box1 box' ref={c => this.container1 = c} />
+				<div className='box2 box' ref={c => this.container2 = c} />
+				<div className='box3 box' ref={c => this.container3 = c} />
+				<div className='box4 box' ref={c => this.container4 = c} />
+				<div className='box5 box' ref={c => this.container5 = c} />
+			</div>
+
+		);
+	}
+}
+
+export default Box;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,47 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Redirect, Link} from 'react-router-dom';
+import {TransitionGroup} from 'react-transition-group'
+import Box from './Box';
 
 class LandingPage extends Component {
+
+	state = {
+		shouldShowBox: true
+	};
+
+	toggleBox = () => {
+		this.setState({
+			shouldShowBox: !this.state.shouldShowBox
+		});
+	}
+
 	render() {
 
 		if(this.props.loggedIn) {
 			return <Redirect to='/dashboard' />;
 		}
 
+		let myHistory = this.props.history;
+
 		return (
+			
 			<div>
-				This is the landing page
-				<div><Link to='/login'>Login</Link></div>
-				<div><Link to='/register'>Register</Link></div>
+				<TransitionGroup>
+					{this.state.shouldShowBox && <Box />}
+				</TransitionGroup>
+				<div className='login_button_container'>
+					<button className='login_link login_button' onClick={e =>
+						{
+							this.toggleBox();
+							setTimeout(function() {myHistory.push('/login');}, 600)}
+						}>
+						Login
+					</button>
+				</div>
+				<div className='register_button'>
+					<Link className='register_link' to='/register'>Register</Link>
+				</div>
 			</div>
 		)
 	}
