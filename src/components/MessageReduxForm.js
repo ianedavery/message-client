5c108d398cf49e0358475003c916079181165f06
Ian Avery <ianedavery@gmail.com>
2018-06-18 08:49:04 -0700

    added style to comment button

--- src/components/MessageReduxForm.js
+++ src/components/MessageReduxForm.js
@@ -18,9 +18,9 @@ class MessageReduxForm extends Component {
 				<label htmlFor='text' aria-label='message'></label>
 				<div className='textarea_container'>
 					<Field className='textarea' component='textarea' type='text' rows='3' cols='25' name='text' />
+				<button className='comment_button' type='submit' disabled={this.props.pristine || this.props.submitting}>Comment</button>
 				</div>
-				<button type='submit' disabled={this.props.pristine || this.props.submitting}>Comment</button>
-
+			
 			</form>
 		);
 	
